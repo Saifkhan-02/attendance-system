634c67a06d4afa6707a7f4364fbe6be93c2fe24f
Saifkhan-02 <saifkhanyt5946@gmail.com>
2026-06-01 17:56:43 +0530

    merge admin login

--- src/main/java/com/gps/attendance/controller/AdminController.java
+++ src/main/java/com/gps/attendance/controller/AdminController.java
@@ -1,43 +1,85 @@
+// package com.gps.attendance.controller;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+// @Controller
+// @RequestMapping("/admin")
+// public class AdminController {
+
+//     @GetMapping("/login")
+//     public String loginPage() {
+//         return "admin-login";
+//     }
+
+//     @PostMapping("/login")
+//     public String login(
+//             @RequestParam String username,
+//             @RequestParam String password) {
+
+//         if ("admin".equals(username)
+//                 && "admin123".equals(password)) {
+
+//             return "admin-dashboard";
+//         }
+          
+
+//         return "admin-login";
+//     }
+//    @GetMapping("/logout")
+// public String logout(jakarta.servlet.http.HttpSession session) {
+
+//     // Session destroy
+//     session.invalidate();
+
+//     return "admin-logout";
+// }
+// }
+
 package com.gps.attendance.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.gps.attendance.entity.Admin;
+import com.gps.attendance.repository.AdminRepository;
 
-
-
-@Controller
+@RestController
+@CrossOrigin("*")
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "admin-login";
-    }
+    @Autowired
+    private AdminRepository repository;
 
     @PostMapping("/login")
-    public String login(
-            @RequestParam String username,
-            @RequestParam String password) {
+    public ResponseEntity<?> login(
+            @RequestBody Admin admin) {
 
-        if ("admin".equals(username)
-                && "admin123".equals(password)) {
+        Admin a =
+                repository.findByUsernameAndPassword(
+                        admin.getUsername(),
+                        admin.getPassword());
 
-            return "admin-dashboard";
+        if(a != null){
+
+            return ResponseEntity.ok(a);
+
         }
-          
 
-        return "admin-login";
+        return ResponseEntity
+                .badRequest()
+                .body("Invalid Admin Credentials");
     }
-   @GetMapping("/logout")
-public String logout(jakarta.servlet.http.HttpSession session) {
-
-    // Session destroy
-    session.invalidate();
-
-    return "admin-logout";
-}
 }
---
+++ src/main/java/com/gps/attendance/repository/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.gps.attendance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gps.attendance.entity.Admin;
+
+public interface AdminRepository
+        extends JpaRepository<Admin, Long> {
+
+    Admin findByUsernameAndPassword(
+            String username,
+            String password);
+}
