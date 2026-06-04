223c806b4bce4f106a1b6b927cedb72f54c98fe2
Saifkhan-02 <saifkhanyt5946@gmail.com>
2026-06-04 20:44:34 +0530

    fix mobile login

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -2,6 +2,6 @@ const BASE_URL =
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
         ? "http://localhost:8080"
-        : "https://attendance-system-2ij3y.onrender.com";
+        : "https://attendance-system-2-ij3y.onrender.com";
 
         // alert("BASE_URL = " + BASE_URL);
