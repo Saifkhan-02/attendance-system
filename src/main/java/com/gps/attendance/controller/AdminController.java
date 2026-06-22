208ed82e50b042067157fa2eb1be655d9351f726
raqibkhan07 <raqib6307khanstp@gmail.com>
2026-06-23 03:51:59 +0530

    some pages add or some pages updates

--- src/main/java/com/gps/attendance/controller/AdminController.java
+++ src/main/java/com/gps/attendance/controller/AdminController.java
@@ -1,47 +1,33 @@
 // package com.gps.attendance.controller;
-
 // import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
-
-
-
-
 // @Controller
 // @RequestMapping("/admin")
 // public class AdminController {
-
 //     @GetMapping("/login")
 //     public String loginPage() {
 //         return "admin-login";
 //     }
-
 //     @PostMapping("/login")
 //     public String login(
 //             @RequestParam String username,
 //             @RequestParam String password) {
-
 //         if ("admin".equals(username)
 //                 && "admin123".equals(password)) {
-
 //             return "admin-dashboard";
 //         }
-          
-
 //         return "admin-login";
 //     }
 //    @GetMapping("/logout")
 // public String logout(jakarta.servlet.http.HttpSession session) {
-
 //     // Session destroy
 //     session.invalidate();
-
 //     return "admin-logout";
 // }
 // }
-
 package com.gps.attendance.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +38,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gps.attendance.dto.AdminChangePasswordRequest;
 import com.gps.attendance.entity.Admin;
 import com.gps.attendance.repository.AdminRepository;
 
@@ -67,12 +54,12 @@ public class AdminController {
     public ResponseEntity<?> login(
             @RequestBody Admin admin) {
 
-        Admin a =
-                repository.findByUsernameAndPassword(
+        Admin a
+                = repository.findByUsernameAndPassword(
                         admin.getUsername(),
                         admin.getPassword());
 
-        if(a != null){
+        if (a != null) {
 
             return ResponseEntity.ok(a);
 
@@ -81,5 +68,42 @@ public class AdminController {
         return ResponseEntity
                 .badRequest()
                 .body("Invalid Admin Credentials");
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(
+            @RequestBody AdminChangePasswordRequest request) {
+
+       System.out.println("Admin ID = " + request.getAdminId());
+System.out.println("Current Password = " + request.getCurrentPassword());
+System.out.println("New Password = " + request.getNewPassword());
+
+        Admin admin
+                = repository.findById(
+                        request.getAdminId())
+                        .orElse(null);
+
+        if (admin == null) {
+
+            return ResponseEntity
+                    .badRequest()
+                    .body("Admin Not Found");
+        }
+
+        if (!admin.getPassword().equals(
+                request.getCurrentPassword())) {
+
+            return ResponseEntity
+                    .badRequest()
+                    .body("Current Password Incorrect");
+        }
+
+        admin.setPassword(
+                request.getNewPassword());
+
+        repository.save(admin);
+
+        return ResponseEntity.ok(
+                "Password Updated Successfully");
     }
 }
--- src/main/java/com/gps/attendance/repository/AdminRepository.java
+++ src/main/java/com/gps/attendance/repository/AdminRepository.java
@@ -10,4 +10,6 @@ public interface AdminRepository
     Admin findByUsernameAndPassword(
             String username,
             String password);
+
+    Admin findByUsername(String username);
 }
