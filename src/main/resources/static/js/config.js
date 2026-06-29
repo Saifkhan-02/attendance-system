9fcc0b04d356d67a97598084a07d520252756ade
Saifkhan-02 <saifkhanyt5946@gmail.com>
2026-06-30 01:37:40 +0530

    Update backend Base url to VPS

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -2,6 +2,11 @@ const BASE_URL =
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
         ? "http://localhost:8080"
-        : "https://attendance-system-2-ij3y.onrender.com";
+        : "http://187.127.152.178:8080";
+// const BASE_URL =
+//     window.location.hostname === "127.0.0.1" ||
+//     window.location.hostname === "localhost"
+//         ? "http://localhost:8080"
+//         : "https://attendance-system-2-ij3y.onrender.com";
 
         // alert("BASE_URL = " + BASE_URL); 
