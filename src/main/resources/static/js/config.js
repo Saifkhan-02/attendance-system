083472d0d438def61c5216971dc9e803a82494e5
Saifkhan-02 <saifkhanyt5946@gmail.com>
2026-07-01 16:11:46 +0530

    Update backend Base url to VPS

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -2,7 +2,7 @@ const BASE_URL =
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
         ? "http://localhost:8080"
-        : "http://187.127.152.178:8080";
+        : window.location.origin;
 // const BASE_URL =
 //     window.location.hostname === "127.0.0.1" ||
 //     window.location.hostname === "localhost"
