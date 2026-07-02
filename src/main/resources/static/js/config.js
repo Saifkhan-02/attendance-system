084ab0fdb91720b67fcfc9b2d8e51c187b4e5d05
raqibkhan07 <raqib6307khanstp@gmail.com>
2026-07-02 20:35:32 +0530

    backup before pull

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -3,10 +3,3 @@ const BASE_URL =
     window.location.hostname === "localhost"
         ? "http://localhost:8080"
         : window.location.origin;
-// const BASE_URL =
-//     window.location.hostname === "127.0.0.1" ||
-//     window.location.hostname === "localhost"
-//         ? "http://localhost:8080"
-//         : "https://attendance-system-2-ij3y.onrender.com";
-
-        // alert("BASE_URL = " + BASE_URL); 
